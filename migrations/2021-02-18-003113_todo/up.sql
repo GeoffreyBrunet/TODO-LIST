8878b14d7df354fcd0a6ac7b82a09bed678f8e81
Geoffrey <geoffrey.brunet@quartz-insight.com>
2021-02-18 08:10:07 +0100

    change to boolean for check or not

--- migrations/2021-02-18-003113_todo/up.sql
+++ migrations/2021-02-18-003113_todo/up.sql
@@ -3,6 +3,10 @@ CREATE TABLE todolist(
   id SERIAL PRIMARY KEY,
   title VARCHAR NOT NULL,
   description VARCHAR NOT NULL,
-  deadline DATE NOT NULL,
-  done INT NOT NULL
+  deadline VARCHAR NOT NULL,
+  done BOOLEAN NOT NULL
 )
+
+INSERT INTO todolist (title, description, deadline, done)
+  VALUES
+    ("First task", "Need to do this", "Demain", FALSE)
