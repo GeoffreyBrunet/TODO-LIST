5f0b889351348ed162230f0275d25891b8fcc9e6
Geoffrey <geoffrey.brunet@quartz-insight.com>
2021-02-18 04:33:30 +0100

    test env var for connection at start, and i'm try to add connection to postgresql

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,63 @@
-#![feature(proc_macro_hygiene, decl_macro)]
-
+#![feature(decl_macro, proc_macro_hygiene)]
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate diesel;
+#[macro_use] extern crate serde_derive;
+extern crate dotenv;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate rocket_contrib;
 
-pub mod cors;
-//pub mod models;
-pub mod routes;
-pub mod schema;
+use dotenv::dotenv;
+use rocket::response::content;
+
+//mod schema;
+//mod connection;
+
+#[get("/")]
+fn index() -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'world' }")
+}
+
+#[get("/todo")]
+fn get_todo() -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo' }")
+}
+
+#[get("/todo/<id>")]
+fn get_todo_by_id(id: u32) -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo id' }")
+}
+
+// GET /todo?date=20210215
+
+/*#[get("/todo?date=<get_date>")]
+fn get_todo_by_date(get_date: u32) -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo get_date' }")
+}*/
+
+#[post("/todo", format = "application/json")]
+fn post_todo() -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo' }")
+}
+
+#[put("/todo/<id>", format = "application/json")]
+fn put_todo(id: u32) -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo' }")
+}
+
+#[delete("/todo/<id>")]
+fn delete_todo(id: u32) -> content::Json<&'static str> {
+    content::Json("{ 'hi': 'todo' }")
+}
 
 fn main() {
+    dotenv().ok();
     rocket::ignite()
-        .mount("/", routes![routes::index,
-        routes::get_todo,
-        routes::get_todo_by_id,
-        routes::post_todo,
-        routes::put_todo,
-        routes::delete_todo])
+        .mount("/", routes![index,
+        get_todo,
+        get_todo_by_id,
+        post_todo,
+        put_todo,
+        delete_todo])
         .launch();
 }
